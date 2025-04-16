d21dac178649ceea152f0b086a04be6f19dbbd4e
cameron bradford <kylocam0712@gmail.com>
2025-04-16 07:58:48 -0500

    Initial Commit

---
+++ loginExperiment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  loginExperiment
+//
+//  Created by CAMERON BRADFORD on 4/16/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ loginExperimentTests/loginExperimentTests.swift
@@ -0,0 +1,17 @@
+//
+//  loginExperimentTests.swift
+//  loginExperimentTests
+//
+//  Created by CAMERON BRADFORD on 4/16/25.
+//
+
+import Testing
+@testable import loginExperiment
+
+struct loginExperimentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
