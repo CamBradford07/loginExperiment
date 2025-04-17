4ff596ddc4819450e08230681cf5674f936b5960
cameron bradford <kylocam0712@gmail.com>
2025-04-17 07:45:06 -0500

    added seque stuff

--- loginExperiment/ViewController.swift
+++ loginExperiment/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameFieldOutlet: UITextField!
+    
+    @IBOutlet weak var passwordFieldOutlet: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitAction(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func newAccountAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "newAccount", sender: self)
+    }
 }
 
---
+++ loginExperiment/ViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController2.swift
+//  loginExperiment
+//
+//  Created by CAMERON BRADFORD on 4/17/25.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
