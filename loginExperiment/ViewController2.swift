56318e005d9bdf612c466856773cbe084fea387b
hi <daniel.huseby26@d155.org>
2025-04-17 07:54:17 -0500

    work on 2nd view controller

--- loginExperiment/ViewController2.swift
+++ loginExperiment/ViewController2.swift
@@ -6,16 +6,29 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 
 class ViewController2: UIViewController {
 
+    
+    @IBOutlet weak var userNameOutlet: UITextField!
+    
+    @IBOutlet weak var passwordOutlet: UITextField!
+    
+    
+    
+    var ref: DatabaseReference!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func createAction(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
