fe5aecc0b3042b6e72779b859578a97b197a0d52
hi <daniel.huseby26@d155.org>
2025-04-17 07:44:24 -0500

    fvbj

--- loginExperiment/ViewController.swift
+++ loginExperiment/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
         // Do any additional setup after loading the view.
     }
 
